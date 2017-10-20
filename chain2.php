4e526a41453682c041030c2f12522bb32ea17e81
maievshadow <fz.uuuu@gmail.com>
2017-10-20 16:36:42 +0800

    链式的v1

---
+++ chain2.php
@@ -0,0 +1,72 @@
+<?php
+class Request{
+
+    private $_data ;
+
+    private $_successor;
+
+    public function __construct($data, $successor){
+        $this->_data = $data;
+        $this->_successor = $successor;
+    }
+}
+
+class Handler{
+
+    protected $_handler; //parent handler
+
+    protected $_successor;
+
+    public function hander(Request $r){
+        if ($this->_successor == 0){
+            $this->_hander->hander($r);
+        }else{
+            printf("end haner\r\n");
+        }
+    }
+
+    protected function hasHander(){
+        return $this->_successor == 1 ? true: false;
+    }
+
+    public function __construct(Handler $h = null , $_successor = 0){
+        $this->_handler = $h;
+        $this->_successor = $_successor;
+    }
+}
+
+class Handler1 extends Handler{
+
+    public function __construct(Handler $h = null, $s= 1){
+        parent::__construct($h,$s);
+    }
+
+    public function hander(Request $r){
+        if ($this->hasHander()){
+            printf("handler1 \r\n");
+        }else{
+            $this->_handler->hander($r);
+        }
+    }
+}
+
+class Handler2 extends Handler{
+
+    public function __construct(Handler $h = null, $s= 2){
+        parent::__construct($h,$s);
+    }
+
+    public function hander(Request $r){
+        if ($this->hasHander()){
+            printf("handler2 \r\n");
+        }else{
+            $this->_handler->hander($r);
+        }
+    }
+}
+
+$request = new Request(null,0);
+$h = new Handler();
+$h1 = new Handler1($h, 1);
+$h2 = new Handler2($h1,0);
+$h2->hander($request); //h1 ??
